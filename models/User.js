bc86247e6b305a9f1d4cefb615111f0e5ce3e7ff
LaupWing <loc_strict@hotmail.com>
2020-03-23 19:53:23 +0100

    models: update User model

--- models/User.js
+++ models/User.js
@@ -48,13 +48,28 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
       },
-      mainPicture: {
-        type: Boolean,
-        required: true,
+      order: {
+        type: Number,
       },
     },
   ],
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign(
+    {
+      _id: user._id.toString(),
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7 days",
+    }
+  );
+  user.tokens = user.tokens.concat({ token });
+  user.save();
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
