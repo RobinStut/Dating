b68e181cfa512ec9588dd87a586c76d13f281662
benl95 <beno_1995@hotmail.com>
2020-03-24 09:37:50 +0100

    added: setup connection mongoose & mongodb

--- server.js
+++ server.js
@@ -3,6 +3,62 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 const exphbs = require("express-handlebars");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// Mongoose connection setup
+const uri =
+  "mongodb+srv://admin:" +
+  process.env.DB_PASS +
+  "@projecttech-a3phf.mongodb.net/test";
+
+mongoose.connect(uri || "mongodb://localhost/playlist", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected");
+});
+
+// MongoDB connection setup
+async function main() {
+  const uri =
+    "mongodb+srv://admin:" +
+    process.env.DB_PASS +
+    "@projecttech-a3phf.mongodb.net/test";
+
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+
+    // Make the appropriate DB calls
+    await listDatabases(client);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+// Database function retrieve list databases
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach((db) => console.log(` - ${db.name}`));
+}
+
+// Handlebars setup
 
 app.engine(
   "handlebars",
@@ -14,8 +70,12 @@ app.engine(
 
 app.set("view engine", "handlebars");
 
+// Routing
+
 app.get("/", (req, res) =>
-  res.render("pages/index", { title: "Your title here" })
+  res.render("pages/index", {
+    title: "Your title here",
+  })
 );
 
 app.use(function (req, res) {
